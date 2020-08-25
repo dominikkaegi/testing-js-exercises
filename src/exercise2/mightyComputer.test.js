1ce650732ccd95593fc246c803cc7b0012537ee5
dominikkaegi <dominik@dominiks-MacBook-Pro.local>
2020-08-25 15:44:15 +0200

    inital commit

---
+++ src/exercise2/mightyComputer.test.js
@@ -0,0 +1,10 @@
+const meaning = require('./mightyComputer')
+
+const { calculateMeaningOfLive } = meaning
+
+
+test.skip('mightyComputer calculates the meaning', async () => {
+})
+
+test.skip('mightyComputer loses focus when disturb', async () => {
+})
