7a08fc497f85d94fe5260a2736d25aa027e2f8d9
dominikkaegi <dominik@dominiks-MacBook-Pro.local>
2020-09-02 17:42:31 +0200

    add create react app template

---
+++ testing-react-components/simple-counter/src/components/Display/Display.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function display({ vlaue }) {
+  return (
+    <div className="display">
+      <span className="display-text">{vlaue}</span>
+    </div>
+  );
+}
