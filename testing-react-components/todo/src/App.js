526565854fb858fa83d24be8e5250a210d3bff27
Saurabh Chauhan <Sau072219@paycor.com>
2020-09-16 00:41:05 -0500

    Added login + routing

--- testing-react-components/todo/src/App.js
+++ testing-react-components/todo/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import Todo from './components/Todo'
+import Routes from './routes';
 
 function App() {
-  return (
-    <div className="App">
-      <Todo />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Routes />
+		</div>
+	);
 }
 
 export default App;
--- testing-react-components/todo/src/utils/api.js
+++ testing-react-components/todo/src/utils/api.js
@@ -1,57 +1,60 @@
-import db from "./db";
+import db from './db';
 
 const REUQEST_DELAY = 200;
 
 const delayedResponse = (data, delay = REUQEST_DELAY) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(data);
-    }, delay);
-  });
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(data);
+		}, delay);
+	});
 };
 const createRandomId = () => {
-  return ~~(Math.random() * 10000000);
+	return ~~(Math.random() * 10000000);
 };
 
 const api = {
-  async getTodos() {
-    const todos = db.getTodos();
-    return delayedResponse(todos, 100);
-  },
-  async createTodo(todoText) {
-    const todo = {
-      text: todoText,
-      id: createRandomId(),
-      isCompleted: false,
-      createdAt: new Date(),
-    };
-    db.createTodo(todo);
-    return delayedResponse(todo);
-  },
-  async updateTodo(todo) {
-    const updatedTodo = {
-      ...todo,
-      updatedAt: new Date(),
-    };
-    db.updateTodo(updatedTodo);
-    return delayedResponse(updatedTodo);
-  },
-  async deleteTodo(todo) {
-    db.deleteTodo(todo);
-    return delayedResponse({
-      id: todo.id,
-    });
-  },
-  async deleteCompleted() {
-    let todos = db.getTodos();
-    const deletedTodos = todos.filter((item) => item.isCompleted);
+	async getTodos() {
+		const todos = db.getTodos();
+		return delayedResponse(todos, 100);
+	},
+	async login(username, password) {
+		return delayedResponse(username === password);
+	},
+	async createTodo(todoText) {
+		const todo = {
+			text: todoText,
+			id: createRandomId(),
+			isCompleted: false,
+			createdAt: new Date()
+		};
+		db.createTodo(todo);
+		return delayedResponse(todo);
+	},
+	async updateTodo(todo) {
+		const updatedTodo = {
+			...todo,
+			updatedAt: new Date()
+		};
+		db.updateTodo(updatedTodo);
+		return delayedResponse(updatedTodo);
+	},
+	async deleteTodo(todo) {
+		db.deleteTodo(todo);
+		return delayedResponse({
+			id: todo.id
+		});
+	},
+	async deleteCompleted() {
+		let todos = db.getTodos();
+		const deletedTodos = todos.filter((item) => item.isCompleted);
 
-    for (let todo of deletedTodos) {
-      db.deleteTodo(todo);
-    }
-    const deletedIds = deletedTodos.map((item) => ({ id: item.id }));
-    return delayedResponse([...deletedIds]);
-  },
+		for (let todo of deletedTodos) {
+			db.deleteTodo(todo);
+		}
+		const deletedIds = deletedTodos.map((item) => ({ id: item.id }));
+		return delayedResponse([...deletedIds]);
+	}
 };
 
 export default api;
