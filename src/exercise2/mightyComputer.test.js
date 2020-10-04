340d84015e646b363da48b1c0dcdbc547f8ca0bb
Dominik Kaegi <kaegidominik@gmail.com>
2020-10-04 10:08:59 +0200

    clean up folder structure

--- src/exercise2/mightyComputer.test.js
+++
@@ -1,7 +0,0 @@
-const meaning = require('./mightyComputer');
-
-const { calculateMeaningOfLive } = meaning;
-
-test('mightyComputer calculates the meaning', async () => {});
-
-test('mightyComputer loses focus when disturb', async () => {});
